64861837234cc8854c2168ae2b9cea86d582970d
yanghajun <hajun.developer@gmail.com>
2024-01-12 14:27:15 +0900

    #7.9~7.10 destination, source, draggableId로 Board 내부, 외부로 이동.

--- src/Components/Board.tsx
+++ src/Components/Board.tsx
@@ -3,10 +3,18 @@ import styled from "styled-components";
 import DragabbleCard from "./DragabbleCard";
 
 const Wrapper = styled.div`
-  padding: 20px 10px;
+  width: 300px;
+  padding-top: 10px;
   background-color: ${(props) => props.theme.boardColor};
   border-radius: 5px;
-  min-height: 200px;
+  min-height: 300px;
+`;
+
+const Title = styled.h2`
+  text-align: center;
+  font-weight: 600;
+  margin-bottom: 10px;
+  font-size: 18px;
 `;
 
 interface IBoardProps {
@@ -16,16 +24,19 @@ interface IBoardProps {
 
 function Board({ toDos, boardId }: IBoardProps) {
   return (
-    <Droppable droppableId={boardId}>
-      {(magic) => (
-        <Wrapper ref={magic.innerRef} {...magic.droppableProps}>
-          {toDos.map((toDo, index) => (
-            <DragabbleCard key={toDo} index={index} toDo={toDo} />
-          ))}
-          {magic.placeholder}
-        </Wrapper>
-      )}
-    </Droppable>
+    <Wrapper>
+      <Title>{boardId}</Title>
+      <Droppable droppableId={boardId}>
+        {(magic) => (
+          <Wrapper ref={magic.innerRef} {...magic.droppableProps}>
+            {toDos.map((toDo, index) => (
+              <DragabbleCard key={toDo} index={index} toDo={toDo} />
+            ))}
+            {magic.placeholder}
+          </Wrapper>
+        )}
+      </Droppable>
+    </Wrapper>
   );
 }
 export default Board;
