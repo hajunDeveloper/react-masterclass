45faf7f6aad809eda4616d101ebdacd3aa1e35b2
yanghajun <hajun.developer@gmail.com>
2024-01-13 13:30:55 +0900

    #6.0~6.4 Framer-Motion, motion.div, aniamte, initial, transition, Variants, stagger, opacity.

--- src/App.tsx
+++ src/App.tsx
@@ -1,68 +1,72 @@
-import {DragDropContext, DropResult} from "react-beautiful-dnd";
 import styled from "styled-components";
-import { useRecoilState } from "recoil";
-import { toDoState } from "./atoms";
-import Board from "./Components/Board";
+import { motion } from "framer-motion";
 
 const Wrapper = styled.div`
-  display: flex;
-  width: 100vw;
-  margin: 0 auto;
-  justify-content: center;
-  align-items: center;
-  height: 100vh;
+height: 100vh;
+width: 100vw;
+display: flex;
+justify-content: center;
+align-items: center;
 `;
 
-const Boards = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: flex-start;
-  width: 100%;
-  gap: 10px;
+const Box = styled(motion.div)`
+  width: 200px;
+  height: 200px;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  background-color: rgba(255, 255, 255, 0.2);
+  border-radius: 40px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-function App() {
-  const [toDos, setToDos] = useRecoilState(toDoState);
-  const onDragEnd = (info: DropResult) => {
-    const { destination, source } = info;
-    if(!destination) return;
-    if(destination?.droppableId === source.droppableId){
-      setToDos((allBoards) => {
-        const boardCopy = [...allBoards[source.droppableId]];
-        const taskObj = boardCopy[source.index];
-        boardCopy.splice(source.index,1);
-        boardCopy.splice(destination?.index, 0, taskObj);
-        return {
-          ...allBoards,
-          [source.droppableId] : boardCopy,
-        };
-      });
-    }
-    if(destination.droppableId !== source.droppableId){
-      setToDos((allBoards) => {
-        const sourceBoard = [...allBoards[source.droppableId]];
-        const taskObj = sourceBoard[source.index];
-        const destinationBoard = [...allBoards[destination.droppableId]];
-        sourceBoard.splice(source.index,1);
-        destinationBoard.splice(destination.index,0, taskObj);
-        return {
-         ...allBoards,
-         [source.droppableId]: sourceBoard,
-         [destination.droppableId]: destinationBoard, 
-        };
-      });
-    }
-  };
+const Circle = styled(motion.div)`
+  background-color: white;
+  height: 70px;
+  width: 70px;
+  place-self: center;
+  border-radius: 35px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
+const boxVariants = {
+  start:{
+    opacity: 0,
+    scale: 0.5,
+  },
+  end: {
+    scale: 1, 
+    opacity: 1,
+    transition: {
+      type: "string",
+      duration: 0.5,
+      bounce: 0.5,
+      delayChildren: 0.5,
+      staggerChildren: 0.2,
+    },
+  },
+};
+
+const circleVariants = {
+  start: {
+    opacity: 0,
+    y:10,
+  },
+  end: {
+    opacity: 1,
+    y:0,
+  },
+};
+
+function App(){
   return(
-    <DragDropContext onDragEnd={onDragEnd}>
-      <Wrapper>
-        <Boards>
-          {Object.keys(toDos).map((boardId)=>(
-            <Board boardId={boardId} key={boardId} toDos={toDos[boardId]}/>
-          ))}
-        </Boards>
-      </Wrapper>
-    </DragDropContext>
+    <Wrapper>
+      <Box variants={boxVariants} initial="start" animate="end">
+        <Circle variants={circleVariants}/>
+        <Circle variants={circleVariants}/>
+        <Circle variants={circleVariants}/>
+        <Circle variants={circleVariants}/>
+      </Box>
+    </Wrapper>
   );
 }
 
