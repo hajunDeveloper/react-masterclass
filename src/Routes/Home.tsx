b4e69e727e104b5cc90983a3eea15383e3b27c44
yanghajun <hajun.developer@gmail.com>
2024-01-20 22:45:50 +0900

    #9.8 Slider(put img)

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -4,9 +4,11 @@ import {motion, AnimatePresence} from "framer-motion";
 import { IGetMoviesResult, getMovies } from "../api";
 import { makeImagePath } from "../utils";
 import { useState } from "react";
+import useWindowDimensions from "../useWindowDimensions";
 
 const Wrapper = styled.div`
-    background: black,
+    background: black;
+    padding-bottom: 200px;
 `;
 
 const Loader = styled.div`
@@ -44,37 +46,40 @@ const Slider = styled.div`
 
 const Row = styled(motion.div)`
   display: grid;
-  gap: 10px;
+  gap: 5px;
   grid-template-columns: repeat(6, 1fr);
   position: absolute;
   width: 100%;
 `;
 
-const Box = styled(motion.div)`
+const Box = styled(motion.div)<{bgPhoto: string}>`
   background-color: white;
+  background-image: url(${(props)=>props.bgPhoto});
+  background-size: cover;
+  background-position: center center;
   height: 200px;
-  color: red;
   font-size: 66px;
 `;
 
-const rowVariants = {
-  hidden: {
-    x: window.outerWidth + 10,
-  },
-  visible: {
-    x: 0,
-  },
-  exit: {
-    x: -window.outerWidth - 10,
-  },
-};
+const offset = 6;
 
 function Home() {
     const {data,isLoading} = useQuery<IGetMoviesResult>(
         ["movies","nowPlaying"], getMovies
     );
     const [index, setIndex] = useState(0);
-    const incraseIndex = () => setIndex((prev) => prev +1);
+    const [leaving, setLeaving] = useState(false);
+    const incraseIndex = () => {
+        if (data){
+            if(leaving) return;
+            toggleLeaving();
+            const totalMovies = data.results.length -1;
+            const maxIndex = Math.floor(totalMovies / offset) -1;
+            setIndex((prev) => (prev===maxIndex ? 0 : prev+1));
+        }
+    };
+    const toggleLeaving = () => setLeaving((prev)=>!prev);
+    const width = useWindowDimensions();
     return (
         <Wrapper>
             {isLoading ? (
@@ -89,18 +94,23 @@ function Home() {
                         <Overview>{data?.results[0].overview}</Overview>
                     </Banner>
                     <Slider>
-                        <AnimatePresence>
+                        <AnimatePresence initial={false} onExitComplete={toggleLeaving}>
                             <Row
-                                variants={rowVariants}
-                                initial="hidden"
-                                animate="visible"
-                                exit="exit"
+                                initial={{ x: width + 5 }}
+                                animate={{ x: 0 }}
+                                exit={{ x: -width - 5 }}
                                 transition={{type: "tween", duration: 1}}
                                 key={index}
                             >
-                                {[1,2,3,4,5,6].map((i)=>(
-                                    <Box key={i}>{i}</Box>
-                                ))}
+                                {data?.results
+                                    .slice(1)
+                                    .slice(offset * index, offset * index + offset)
+                                    .map((movie) => (
+                                        <Box
+                                            key={movie.id}
+                                            bgPhoto={makeImagePath(movie.backdrop_path, "w500")}
+                                        />
+                                    ))}
                             </Row>
                         </AnimatePresence>
                     </Slider>
