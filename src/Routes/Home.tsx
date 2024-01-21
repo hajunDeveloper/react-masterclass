e3316ba77f419def8834c37573341aa0470b7abc
yanghajun <hajun.developer@gmail.com>
2024-01-21 19:11:57 +0900

    #9.13 이미 찾은  Movies 내에서 원하는 Moview  찾기.

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -98,7 +98,34 @@ const BigMovie = styled(motion.div)`
     left: 0;
     right: 0;
     margin: 0 auto;
+    border-radius: 15px;
+    overflow: hidden;
+    background-color: ${(props) => props.theme.black.lighter};
+  `;
+  
+
+const BigCover = styled.div`
+  width: 100%;
+  background-size: cover;
+  background-position: center center;
+  height: 400px;
 `;
+
+const BigTitle = styled.h3`
+  color: ${(props) => props.theme.white.lighter};
+  padding: 20px;
+  font-size: 46px;
+  position: relative;
+  top: -80px;
+`;
+
+const BigOverview = styled.p`
+  padding: 20px;
+  position: relative;
+  top: -80px;
+  color: ${(props) => props.theme.white.lighter};
+`;
+
 
 const boxVariants = {
     normal: {
@@ -151,6 +178,7 @@ function Home() {
         history.push(`/movies/${movieId}`);
     }
     const onOverlayClick = () => history.push("/");
+    const clickedMovie = bigMovieMatch?.params.movieId && data?.results.find((movie)=>movie.id === +bigMovieMatch.params.movieId);
     const width = useWindowDimensions();
     return (
         <Wrapper>
@@ -208,7 +236,17 @@ function Home() {
                                     style={{top: scrollY.get() + 100}}
                                     layoutId={bigMovieMatch.params.movieId}
                                 >
-                                    hello
+                                    {clickedMovie && (
+                                        <>
+                                            <BigCover
+                                                style={{
+                                                    backgroundImage:`linear-gradient(to top, black, transparent), url(${makeImagePath(clickedMovie.backdrop_path, "w500")})`
+                                                }}
+                                            />
+                                            <BigTitle>{clickedMovie.title}</BigTitle>
+                                            <BigOverview>{clickedMovie.overview}</BigOverview>
+                                        </>
+                                    )}
                                 </BigMovie>
                             </>
                         ) : null}
