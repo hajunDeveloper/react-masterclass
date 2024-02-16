8609a72de029cef4da1c24cc085de5a12e5c7ca3
yanghajun <hajun.developer@gmail.com>
2024-02-17 08:51:19 +0900

    #Page Mention Update

--- src/Routes/Search.tsx
+++ src/Routes/Search.tsx
@@ -1,9 +1,15 @@
 import { useLocation } from "react-router-dom";
+import styled from "styled-components";
+
+const Overview = styled.p`
+  margin-top: 100px;
+  font-size: 25px;
+  width: 50%;
+`;
 
 function Search() {
     const location = useLocation();
     const keyword = new URLSearchParams(location.search).get("keyword");
-    console.log(keyword);
-    return null;
+    return <Overview>해당 페이지는 준비 중에 있습니다.<br/>검색한 제목은 '{keyword}'입니다.</Overview>;
 }
 export default Search;
--- src/Routes/Tv.tsx
+++ src/Routes/Tv.tsx
@@ -1,4 +1,12 @@
+import styled from "styled-components";
+
+const Overview = styled.p`
+  margin-top: 100px;
+  font-size: 25px;
+  width: 50%;
+`;
+
 function Tv() {
-    return <h1></h1>;
+    return <Overview>해당 페이지는 준비 중에 있습니다.<br/>'Home'탭과 동일한 방식으로 구성 예정입니다.</Overview>;
 }
 export default Tv;
